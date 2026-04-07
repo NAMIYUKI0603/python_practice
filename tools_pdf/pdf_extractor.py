58f906c53679d40625ac44779d48ab76396467af
NAMIYUKI0603 <tositositosiyuki.4.4.4.4@gmail.com>
2026-04-07 17:01:45 +0900

    paburi

--- tools_pdf/pdf_extractor.py
+++ tools_pdf/pdf_extractor.py
@@ -2,8 +2,8 @@ import pdfplumber
 import os
 
 # --- 設定エリア ---
-INPUT_PDF = "input/職場のあんぜんサイト：化学物質：　酸化アルミニウム.pdf"  # 読み込む白書のPDF
-OUTPUT_TXT = "input/職場のあんぜんサイト：化学物質：　酸化アルミニウム.txt"    # 出力するテキストファイル
+INPUT_PDF = "input/今後の産業廃棄物処理についてのパブリックコメント.pdf"  # 読み込む白書のPDF
+OUTPUT_TXT = "input/今後の産業廃棄物処理についてのパブリックコメント.txt"    # 出力するテキストファイル
 
 print(f"[{INPUT_PDF}] のテキスト抽出を開始します...")
 
